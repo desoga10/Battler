18c0e7e00462ec6065b3c8630a33bd1d4ade08ec
deji adesoga <done2stay@gmail.com>
2020-08-13 23:33:26 +0100

    Started Work On Navbar

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -3,13 +3,33 @@ import React from 'react'
 
 export default class  Popular extends React.Component {
 
+        constructor(props){
+          super(props)
+
+          this.state = {
+            selectedLanguage: 'All'
+          }
+
+          this.updateLanguage = this.updateLanguage.bind(this)
+        }
+
+        updateLanguage(selectedLanguage) {
+          this.setState({
+            selectedLanguage
+          })
+        }
+
          render() {
           const languages = ["All", "javascript", "Ruby", "Java", "Css", "Python"];       
         return (
           <ul className="flex-center">
            { languages.map( (language) => 
           <li key={language}>
-            <button className="btn-clear nav-link">
+            <button 
+              className="btn-clear nav-link"
+              style={language === this.state.selectedLanguage ? { color: 'pink'} : null}
+              onClick={() => this.updateLanguage(language)}
+              >
               {language}
             </button>
           </li>
--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ import Popular from './components/Popular'
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <Popular />
       </div>
     )
