7f39e49916fd1e716a88245124a7b71c7892aeca
deji adesoga <done2stay@gmail.com>
2020-08-13 15:03:59 +0100

    Styled Page

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Popular from './components/Popular'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello World
+        <Popular />
       </div>
     )
   }
 }
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
