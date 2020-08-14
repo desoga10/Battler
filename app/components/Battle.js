d68960dbc8121b29999631ee366c9f7647ef1894
deji adesoga <done2stay@gmail.com>
2020-08-14 15:19:35 +0100

    Started Working On Instruction Component

---
+++ app/components/Battle.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+function Instructions() {
+  return (
+    <div>
+      Instructions
+    </div>
+  )
+}
+
+export default class Battle extends React.Component {
+
+render(){
+  return (
+    <React.Fragment>
+      <Instructions />
+    </React.Fragment>
+  )
+}
+
+}
