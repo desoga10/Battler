f3491c27cdab2013bb928df2220b3eaff43c3116
deji adesoga <done2stay@gmail.com>
2020-08-19 15:48:38 +0100

    Started Working On Navigation

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import Popular from './components/Popular'
 import Battle from './components/Battle'
 import { ThemeProvider } from './context/theme'
 import Nav from './components/Nav'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends React.Component {
   constructor(props) {
@@ -23,14 +24,18 @@ class App extends React.Component {
 
   render() {
     return (
-    <ThemeProvider value={this.state}>
-       <div className={this.state.theme}>
-        <div className="container">
-        <Nav />
-        <Popular/>
-        </div>
-       </div>
-    </ThemeProvider>
+    <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+            <Nav />
+
+            <Route exact path="/" component={Popular} />
+            <Route path="/battle" component={Battle} />
+            </div>
+          </div>
+        </ThemeProvider>
+    </Router>
     )
   }
 }
