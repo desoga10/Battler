48b8655c456809f7a7600fb0060e66c3cbf54ed8
deji adesoga <done2stay@gmail.com>
2020-08-19 19:51:38 +0100

    Added Active Class To Navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import {ThemeConsumer} from '../context/theme'
+import {NavLink} from 'react-router-dom';
+
+const activeStyle = {
+  color: 'red'
+}
 
 export default function Nav () {
   return (
     <ThemeConsumer>
       { ({theme, toggleTheme}) => (
         <nav className="row space-between">
+          <ul className="row-nav">
+              <li className="spacer">
+                <NavLink 
+                    to="/" 
+                    exact
+                    activeStyle={activeStyle}
+                    className="nav-link">
+                    Popular
+                </NavLink>
+              </li>
+              <li>
+                <NavLink 
+                    to="/battle"
+                    activeStyle={activeStyle} 
+                    className="nav-link">
+                      Battle
+                </NavLink>
+              </li>
+          </ul>
+
+
+
           <button
             style={{fontSize: 30}}
             className="btn-clear"
