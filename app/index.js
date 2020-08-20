c6b4e791d40b3d34b8c3e7b428ef021d708b8e70
deji adesoga <done2stay@gmail.com>
2020-08-20 22:34:57 +0100

    Added Dynamic Routing

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Popular from './components/Popular'
-import Battle from './components/Battle'
-import Results from './components/Results';
 import { ThemeProvider } from './context/theme'
 import Nav from './components/Nav'
+import Loading from './components/Loading'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+const Popular = React.lazy( () => import('./components/Popular'))
+const Battle = React.lazy( () => import('./components/Battle'))
+const Results = React.lazy( () => import('./components/Results'))
 
 class App extends React.Component {
  
@@ -26,13 +28,16 @@ class App extends React.Component {
         <ThemeProvider value={this.state}>
           <div className={this.state.theme}>
             <div className="container">
+
             <Nav />
+            <React.Suspense fallback={<Loading />}>
               <Switch>
                 <Route exact path="/" component={Popular} />
                 <Route exact path="/battle" component={Battle} />
                 <Route path="/battle/results" component={Results} />
                 <Route render={() => <h1>404</h1>} />
               </Switch>
+            </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
